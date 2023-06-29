bfade2d872231beb4f71ffadd5d7b5808d24221e
ShoumoPal <arka.pal21@gmail.com>
2023-06-29 18:53:13 +0530

    Added coin insertion logic

--- Connect4Game.cpp
+++ Connect4Game.cpp
@@ -11,14 +11,36 @@ class Connect4Game {
 public:
     void Play() {
 
-        DrawBoard(); // Draws the board
+        bool gameFinished = false;
+        while (!gameFinished) {
+            DrawBoard(); // Draws the board
+            cout << "===================================" << endl;
+            int columnChosen = ChooseColumn();
+            if (isColumnValid(columnChosen)) {
+                int row =  dropPiece(columnChosen);
+                if (checkWin(row, columnChosen)) {
+                    DrawBoard();
+                    cout << "Congratulations! Player " << currentPlayer << " wins!!" << endl;
+                    gameFinished = true;
+                }
+                SwitchPlayer();
+            }
+            else {
+                cout << "Invalid placement, please enter the column number again..." << endl;
+            }
+        }
     }
 
 private:
     Player board[ROW][COL] = { Player::NONE };
+    Player currentPlayer = Player::RED;
 
     void DrawBoard() {
 
+        for (int i = 0; i < COL; i++) {
+            cout << " " << i;
+        }
+        cout << endl;
         cout << "---------------" << endl;
         for (int i = 0; i < ROW; i++) {
             cout << "|";
@@ -39,6 +61,38 @@ private:
             cout << endl;
             cout << "---------------" << endl;
         }
+    }
+
+    void SwitchPlayer() {
+        currentPlayer = (currentPlayer == Player::RED) ? Player::BLUE : Player::RED;
+    }
+
+    int ChooseColumn() {
+        int column;
+        cout << ((currentPlayer == Player::RED) ? "Player 1: " : "Player 2: ") << endl;
+        cout << "Enter the column number (0-6): ";
+        cin >> column;
+        return column;
+    }
+
+    bool isColumnValid(int column) {
+        return (column >= 0 && column < COL && board[0][column] == Player::NONE);
+    }
+
+    int dropPiece(int col) {
+        
+        int row = ROW - 1;
+
+        while (board[row][col] != Player::NONE) {
+            row--;
+        }
+
+        board[row][col] = currentPlayer;
+        return row;
+    }
+
+    bool checkWin(int _row, int _col) {
+        return false;
     }
 };
 int main() {
