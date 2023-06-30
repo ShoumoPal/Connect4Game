5470f06215f7e27ac21e572b6ac92eb5e952fabe
ShoumoPal <arka.pal21@gmail.com>
2023-06-30 23:02:13 +0530

    Added the check win condition as well as polished the look

--- Connect4Game.cpp
+++ Connect4Game.cpp
@@ -11,6 +11,8 @@ class Connect4Game {
 public:
     void Play() {
 
+        ShowInstructions();
+
         bool gameFinished = false;
         while (!gameFinished) {
             DrawBoard(); // Draws the board
@@ -21,6 +23,11 @@ public:
                 if (checkWin(row, columnChosen)) {
                     DrawBoard();
                     cout << "Congratulations! Player " << currentPlayer << " wins!!" << endl;
+                    gameFinished = true;
+                }
+                else if (checkDraw()) {
+                    DrawBoard();
+                    cout << "It's a draw!" << endl;
                     gameFinished = true;
                 }
                 SwitchPlayer();
@@ -35,11 +42,29 @@ private:
     Player board[ROW][COL] = { Player::NONE };
     Player currentPlayer = Player::RED;
 
+    void ShowInstructions() {
+        cout << "--------------------------------------\n" << endl;
+        cout << "\t Welcome to Connect 4\n" << endl;
+        cout << "--------------------------------------\n" << endl;
+        cout << "Rules:\n" << endl;
+        cout << "1. Two-player game (Red & Blue)\n2. Board size is 6x7\n3. A player wins if they are able to connect 4 coins vertically, horizontal and diagonally\n4. Draw when the matrix is filled fully." << endl;
+        cout << "--------------------------------------\n" << endl;
+        cout << "How to Play:\n" << endl;
+        cout << "1. Game always starts with Player 1(Red)\n2. In each step, choose the column number to put your coin in.\n3. Rows will be filled from bottom to top.\n" << endl;
+        cout << "--------------------------------------\n" << endl;
+    }
+
+    bool checkDraw() {
+        for (int j = 0; j < COL; j++) {
+            if (board[0][j] == Player::NONE) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     void DrawBoard() {
 
-        for (int i = 0; i < COL; i++) {
-            cout << " " << i;
-        }
         cout << endl;
         cout << "---------------" << endl;
         for (int i = 0; i < ROW; i++) {
@@ -61,6 +86,10 @@ private:
             cout << endl;
             cout << "---------------" << endl;
         }
+        for (int i = 0; i < COL; i++) {
+            cout << " " << i;
+        }
+        cout << endl;
     }
 
     void SwitchPlayer() {
@@ -92,6 +121,65 @@ private:
     }
 
     bool checkWin(int _row, int _col) {
+        //Horizontal win
+        int count = 0;
+        for (int i = 0; i < COL; i++) {
+            if (board[_row][i] == currentPlayer) {
+                count++;
+                if (count == 4) {
+                    return true;
+                }
+            }
+            else {
+                count = 0;
+            }
+        }
+
+        //Vertical win
+        count = 0;
+        for (int i = 0; i < ROW; i++) {
+            if (board[i][_col] == currentPlayer) {
+                count++;
+                if (count == 4) {
+                    return true;
+                }
+            }
+            else {
+                count = 0;
+            }
+        }
+
+        //Top-left to bottom right check
+        count = 0;
+        int startrow = _row - min(_row, _col);
+        int startcol = _col - min(_row, _col);
+        for (int i = startrow, j = startcol; i < ROW && j < COL; i++, j++) {
+            if (board[i][j] == currentPlayer) {
+                count++;
+                if (count == 4) {
+                    return true;
+                }
+            }
+            else {
+                count = 0;
+            }
+        }
+
+        //Top-right to bottom left check
+        count = 0;
+        startrow = _row + min(ROW - _row - 1, _col);
+        startcol = _col - min(ROW - _row - 1, _col);
+        for (int i = startrow, j = startcol; i < ROW && j < COL; i++, j++) {
+            if (board[i][j] == currentPlayer) {
+                count++;
+                if (count == 4) {
+                    return true;
+                }
+            }
+            else {
+                count = 0;
+            }
+        }
         return false;
     }
 };
